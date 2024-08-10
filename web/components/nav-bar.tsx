23a7e0c32f08af36be32ad342711b595a2e72fcf
Arthur Wang <codecwang@outlook.com>
2024-08-10 23:02:38 +0800

    select menu based on pathname

--- web/components/nav-bar.tsx
+++ web/components/nav-bar.tsx
@@ -1,40 +1,57 @@
 import SearchIcon from "@/icons/search-icon";
+import clsx from "clsx";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
+  const pathname = usePathname();
   return (
-    <nav className="glass fixed bottom-4 left-1/2 -translate-x-1/2 transform rounded-full">
-      <ul className="menu menu-horizontal flex items-center">
-        <li>
-          <Link className="rounded-full" href="/photos">
-            {/* <PhotoIcon className="size-4" /> */}
-            Photos
-          </Link>
-        </li>
-        <li>
-          <Link className="rounded-full" href="/albums">
-            {/* <FolderOpenIcon className="size-4" /> */}
-            Albums
-          </Link>
-        </li>
-        <li>
-          <button className="btn btn-circle btn-ghost btn-sm">
-            <SearchIcon className="size-5" />
-          </button>
-        </li>
-        <li>
-          <Link className="rounded-full" href="/explorer">
-            {/* <GlobeAsiaAustraliaIcon className="size-4" /> */}
-            Explorer
-          </Link>
-        </li>
-        <li>
-          <Link className="rounded-full" href="/my">
-            {/* <UserIcon className="size-4" /> */}
-            My
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="absolute bottom-0 flex w-full flex-col items-center">
+      <nav className="mb-4 rounded-box bg-base-200 shadow-2xl">
+        <ul className="menu menu-horizontal flex items-center">
+          <li>
+            <Link
+              className={clsx(pathname.startsWith("/photos") && "active")}
+              href="/photos"
+            >
+              {/* <PhotoIcon className="size-4" /> */}
+              Photos
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={clsx(pathname.startsWith("/albums") && "active")}
+              href="/albums"
+            >
+              {/* <FolderOpenIcon className="size-4" /> */}
+              Albums
+            </Link>
+          </li>
+          <li>
+            <button className="btn btn-circle btn-ghost btn-sm">
+              <SearchIcon className="size-5" />
+            </button>
+          </li>
+          <li>
+            <Link
+              className={clsx(pathname.startsWith("/explorer") && "active")}
+              href="/explorer"
+            >
+              {/* <GlobeAsiaAustraliaIcon className="size-4" /> */}
+              Explorer
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={clsx(pathname.startsWith("/my") && "active")}
+              href="/my"
+            >
+              {/* <UserIcon className="size-4" /> */}
+              My
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
